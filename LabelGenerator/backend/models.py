965ba7dff29427f4f2fdf05738c63cb080add864
panzhongjie <1275389939@qq.com>
2021-11-21 22:12:53 +0800

    Create models.

--- LabelGenerator/backend/models.py
+++ LabelGenerator/backend/models.py
@@ -4,9 +4,23 @@ from django.db import models
 
 
 # Create your models here.
-class Book(models.Model):
-    book_name = models.CharField(max_length=64)
-    add_time = models.DateTimeField(auto_now_add=True)
+class SpanInformation(models.Model):
+    trace_start_timestamp = models.CharField(max_length=64)
+    trace_id = models.CharField(max_length=64)
+    span_id = models.CharField(max_length=64)
+    parent_span_id = models.CharField(max_length=64)
+    span_start_timestamp = models.CharField(max_length=64)
+    duration = models.CharField(max_length=64)
+    operation_name = models.CharField(max_length=150)
+    service_name = models.CharField(max_length=64)
 
     def __unicode__(self):
-        return self.book_name
+        return self.span_id
+
+
+class TraceInformation(models.Model):
+    trace_id = models.CharField(max_length=64)
+    label = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.trace_id
--- LabelGenerator/backend/urls.py
+++ LabelGenerator/backend/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 import backend.views as views
 
 urlpatterns = [
-    path('add_books/', views.add_book),
-    path('show_books/', views.show_books),
+
 ]
